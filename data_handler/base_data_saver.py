76f006b54146d7071fc0fb95eeaacaf9b1bbb278
katty87 <esenenkova@gmail.com>
2022-12-29 23:34:42 +0300

    Add docstrings

--- data_handler/base_data_saver.py
+++ data_handler/base_data_saver.py
@@ -1,13 +1,24 @@
 class BaseDataSaver:
+    """Base class for data save functionality"""
     @staticmethod
     def get_type() -> str:
+        """
+        return data saver type
+        :rtype: str
+        """
         raise NotImplementedError()
 
     def init(self):
         raise NotImplementedError()
 
     def finalize(self):
+        """
+        Close and clear all resources
+        """
         raise NotImplementedError()
 
     def save_data_item(self, data: dict):
+        """
+        Save data to storage
+        """
         raise NotImplementedError()
