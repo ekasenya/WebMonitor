1282bb3b82e8030c3f26844d428b4431c8a3c39d
katty87 <esenenkova@gmail.com>
2022-12-29 14:54:30 +0300

    Add package name to imports

--- data_handler/data_saver.py
+++ data_handler/data_saver.py
@@ -1,22 +1,33 @@
-from data_saver_constants import DataSaverTypes
-from postgre_sql_data_saver import PostgreSqlDataSaver
-from base_data_saver import BaseDataSaver
+#!/usr/bin/env python3
+
+import logging
+
+from data_handler.data_saver_constants import DataSaverTypes
+from data_handler.postgre_sql_data_saver import PostgreSqlDataSaver
+from data_handler.base_data_saver import BaseDataSaver
 
 
-def init_data_saver(data_saver_type: str) -> BaseDataSaver:
+logger = logging.getLogger('data_saver')
+
+
+def init_data_saver(data_saver_type: str, config: dict) -> BaseDataSaver:
     """
     Create an object for save data metrics
 
+    :param config: configuration
     :type data_saver_type: str
     :rtype: BaseDataSaver
     """
 
-    print('init_data_saver')
+    logger.info('Start creating data saver. Type: {}'.format(data_saver_type))
+
     if not DataSaverTypes.has_value(data_saver_type):
         raise TypeError()
 
     data_saver = None
     if data_saver_type == DataSaverTypes.POSTGRE_SQL.value:
-        data_saver = PostgreSqlDataSaver()
+        data_saver = PostgreSqlDataSaver(config)
+
+    logger.info('Data saver is created')
 
     return data_saver
