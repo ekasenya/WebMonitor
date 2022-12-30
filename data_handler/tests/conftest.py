c826b6c2458874831799985c6147fa14793fe413
katty87 <esenenkova@gmail.com>
2022-12-30 03:00:04 +0300

    Add mocks

--- data_handler/tests/conftest.py
+++ data_handler/tests/conftest.py
@@ -1,20 +1,44 @@
+import mock
 import pytest
 
 from data_handler.postgre_sql_data_saver import PostgreSqlDataSaver
 
 
 @pytest.fixture(scope="module")
-# use mock for simplify test. It should be replaced with testing db
-def db_connection():
-    pass
+def args():
+    return mock.Mock()
 
 
 @pytest.fixture(scope="module")
-# use mock for simplify test. It should be replaced with testing db
-def postgre_sql_data_saver():
-    return PostgreSqlDataSaver(
-        {'connection': {'dbname': 'test_db', 'host': '127.0.0.1', 'port': '5432', 'user': 'user',
-                        'password': 'password'},
-         'reconnect': {'tries': 3, 'delay': 1, 'backoff': 2}
-         }
-    )
+def config() -> dict:
+    return {
+        'kafka_consumer': {
+            'topic': 'web_monitor_topic',
+            'sleep_interval': 5,
+            'connection': {
+                'bootstrap_servers': 'broker: 9092',
+                'group_id': 'web_monitor_group_1',
+                'consumer_timeout_ms': '10000'
+            }
+        },
+        'postgre_sql': {
+            'connection': {
+                'host': 'localhost',
+                'port': 5432,
+                'dbname': 'postgres',
+                'user': 'postgres',
+                'password': 'postgres'
+            },
+            'reconnect': {
+                'tries': 5,
+                'delay': 1,
+                'backoff': 2
+            }
+        }
+    }
+
+
+@pytest.fixture(scope="module")
+# TODO: create and init db
+def postgre_sql_data_saver() -> PostgreSqlDataSaver:
+    return PostgreSqlDataSaver()
